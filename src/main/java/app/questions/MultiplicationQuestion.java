82cb9e6fbdb231100f4b73c34fc96d98a4258c57
tonu0226 <tonuci.ryan@gmail.com>
2023-05-29 10:35:03 -0400

    commit

---
+++ src/main/java/app/questions/MultiplicationQuestion.java
@@ -0,0 +1,35 @@
+package app.questions;
+
+import java.util.Random;
+
+public class MultiplicationQuestion implements Question{
+    private int operand1;
+    private int operand2;
+    public Question(Random rand) {
+        this.operand1 = rand.nextInt(0, 12);
+        this.operand2 = rand.nextInt(0, 12);
+    }
+    @Override
+    public String getPrompt() {
+        return "What is %d x %d? ".formatted(operand1, operand2);
+    }
+    @Override
+    public String getValidityMessage() {
+        return "Your answer must be a number";
+    }
+    @Override
+    public String parseAnswer(String answer) {
+        try {
+            double answerAsDouble = Double.parseDouble(answer);
+
+            if (answerAsDouble == (double)(operand1 * operand2)) {
+                return ANSWER_CORRECT;
+            }
+            return ANSWER_INCORRECT;
+
+        } catch (NumberFormatException e) {
+            return ANSWER_INVALID;
+        }
+    }
+
+}
