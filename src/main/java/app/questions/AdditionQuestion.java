423df4ab3e0dfb619e22273c6cbbbb3157f65bb9
tonu0226 <tonuci.ryan@gmail.com>
2023-05-30 09:43:27 -0400

    starting 5

--- src/main/java/app/questions/AdditionQuestion.java
+++ src/main/java/app/questions/AdditionQuestion.java
@@ -8,7 +8,7 @@ public class AdditionQuestion extends ArithmeticQuestion{
         operand1 = rand.nextInt(0, 12);
         operand2 = rand.nextInt(0, 12);
         operatorSymbol = '+';
-    }
+    } //still not passing getPrompt() test (none of the questions are)
     public double calculateAnswer(){
         return operand1 + operand2;
     }
