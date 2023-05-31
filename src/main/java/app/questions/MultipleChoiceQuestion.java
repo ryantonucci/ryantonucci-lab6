f4d501dc84957b7c77cfc5842023459ec7e9fb7c
tonu0226 <tonuci.ryan@gmail.com>
2023-05-31 11:01:57 -0400

    starting 7

--- src/main/java/app/questions/MultipleChoiceQuestion.java
+++ src/main/java/app/questions/MultipleChoiceQuestion.java
@@ -1,13 +1,21 @@
 package app.questions;
 
-public class MultipleChoiceQuestion {
+public class MultipleChoiceQuestion implements Question {
 
     /**
      * @param questionText The main question text
      * @param choices An array containing the text of the various choices for the question
      * @param correctChoiceIdx The index of the correct answer in the given choices array
      */
+    private String questionText;
+    private String[] choices;
+    private int correctChoiceIdx;
     public MultipleChoiceQuestion(String questionText, String[] choices, int correctChoiceIdx) {
+        this.questionText = questionText;
+        this.choices = choices;
+        this.correctChoiceIdx = correctChoiceIdx;
+    }
+    public String getPrompt(){
 
     }
 }
