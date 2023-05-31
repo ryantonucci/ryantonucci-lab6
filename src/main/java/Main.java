b2f7c96d3bf06f516e4b14a70cfbd835d59182db
tonu0226 <tonuci.ryan@gmail.com>
2023-05-31 10:28:40 -0400

    starting 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,14 @@ import app.App;
 import app.questions.QuestionFactory;
 import ui.ConsoleUI;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         ConsoleUI ui = new ConsoleUI(new Scanner(System.in), System.out);
-        QuestionFactory questionFactory = new QuestionFactory();
+        QuestionFactory questionFactory = new QuestionFactory(new Random());
         App app = new App(ui, questionFactory);
         app.run();
     }
--- src/main/java/app/questions/QuestionFactory.java
+++ src/main/java/app/questions/QuestionFactory.java
@@ -8,18 +8,30 @@ import java.util.Random;
  * Creates sets of questions to ask in a quiz
  */
 public class QuestionFactory {
-
+    private Random rand;
     /**
      * @param n The number of questions to create
      * @return An array of n questions
      */
+
     public Question[] makeQuestions(int n) {
         Question[] questions = new Question[n];
 
         for ( int i = 0; i < n ; i++ ) {
-            questions[i] = new MultiplicationQuestion(new Random());
-        }
+            int choice = rand.nextInt(0, 5);
 
+            if (choice == 0) {
+                questions[i] = new AdditionQuestion(rand);
+            } else if (choice == 1) {
+                questions[i] = new SubtractionQuestion(rand);
+            } else if (choice == 2) {
+                questions[i] = new MultiplicationQuestion(rand);
+            } else if (choice == 3) {
+                questions[i] = new DivisionQuestion(rand);
+            } else {
+                questions[i] = new MultipleChoiceQuestion(rand);
+            }
+        }
         return questions;
     }
 }
